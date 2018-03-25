1885c095ef2681fe201966cca0a7486f5d216c13
muhammad-alaref <muhammad.alaref96@eng-st.cu.edu.eg>
2018-03-25 16:03:38 +0200

    Refactored the indexer, multi-threading and graph updating!

---
+++ src/main/java/indexer/Stemmer.java
@@ -0,0 +1,38 @@
+package indexer;
+
+import opennlp.tools.stemmer.PorterStemmer;
+import util.PathGenerator;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Stemmer implements Runnable {
+
+    final private int urlId;
+
+    Stemmer(int urlId) {
+        this.urlId = urlId;
+    }
+
+    @Override
+    public void run() {
+        try {
+            String html = new String(Files.readAllBytes(PathGenerator.generate("HTML", String.valueOf(urlId))));
+            String[] words = html.replaceAll("[^\\p{L} ]", " ").toLowerCase().split("\\s+");
+            PorterStemmer porterStemmer = new PorterStemmer();
+            List<String> stemmedWords = new LinkedList<>();
+            for (String word : words) {
+                String stemmedWord = porterStemmer.stem(word);
+                if (stemmedWord.length() > 1) {
+                    stemmedWords.add(stemmedWord);
+                }
+            }
+            // TODO add stemmed words to DB instead of printing them
+            System.out.println(stemmedWords);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
