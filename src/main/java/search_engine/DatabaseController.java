16efca5f72d2de93a865caee55c6931c726f3e0e
MK <mahmoud.k.nasr@gmail.com>
2018-04-22 22:06:07 +0200

    Fix the sorting bug in search_engine DatabaseController

--- src/main/java/search_engine/DatabaseController.java
+++ src/main/java/search_engine/DatabaseController.java
@@ -1,14 +1,23 @@
 package search_engine;
 
-import model.Document;
 import model.URL;
 import util.DatabaseDriver;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class DatabaseController {
 
     static List<URL> getUrls (Iterable<Integer> urls){
-        return DatabaseDriver.datastore.createQuery(URL.class).field("urlId").in(urls).asList();
+        List<URL> urlList = DatabaseDriver.datastore.createQuery(URL.class).field("urlId").in(urls).asList();
+        List<URL> sortedUrlList = new LinkedList<>();
+        for (Integer urlId : urls){
+            for (URL url : urlList) {
+                if (urlId.equals(url.getUrlId())){
+                    sortedUrlList.add(url);
+                }
+            }
+        }
+        return sortedUrlList;
     }
 }
